d2ef2e72e74300570a454bd34871054b4ea9636b
pradip mandal <join2me.pm@gmail.com>
2023-12-27 07:45:54 +0530

    doc

--- Dutch-National-Flag/rgb-segregate.js
+++ Dutch-National-Flag/rgb-segregate.js
@@ -31,8 +31,8 @@ const rgbSegregate = (arr) => {
 // This problem can be solved using the Dutch National Flag problem solution. The idea is to use three pointers to separate the different types of elements.
 
 //explain
-/*
-This function works by maintaining three pointers: low, mid, and high. The low pointer is used to track the position where the next 'R' character should be placed, the mid pointer is used to track the position where the next 'G' character should be placed, and the high pointer is used to track the position where the next 'B' character should be placed.
+/**
+ This function works by maintaining three pointers: low, mid, and high. The low pointer is used to track the position where the next 'R' character should be placed, the mid pointer is used to track the position where the next 'G' character should be placed, and the high pointer is used to track the position where the next 'B' character should be placed.
 
 The function iterates over the array, and for each character, it does the following:
 
@@ -40,7 +40,7 @@ If the character is 'R', it swaps the character at the mid pointer with the char
 If the character is 'G', it doesn't need to do anything (since 'G' is in the correct position), so it just increments the mid pointer.
 If the character is 'B', it swaps the character at the mid pointer with the character at the high pointer, and then decrements the high pointer (but doesn't increment the mid pointer, since the new character at the mid position hasn't been processed yet).
 This ensures that all 'R' characters are placed before all 'G' characters, and all 'G' characters are placed before all 'B' characters, thus segregating the array as required. The time complexity of this function is O(n), where n is the length of the array, and it uses O(1) extra space, so it meets the problem's requirements.
-*/
+ */
 const rgbSegregateDNF = (arr) => {
   let low = 0;
   let mid = 0;
